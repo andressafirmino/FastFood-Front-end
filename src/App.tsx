a127bb89bf3f7ffeefd20e56e891fbc6a51e01ae
andressafirmino <andressafirmino78@gmail.com>
2023-12-15 19:18:32 -0300

    feat: withdrawal route created

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Order from "./pages/OrderPage/OrderPage";
 import Kitchen from "./pages/KitchenPage/KitchenPage";
+import Withdrawal from "./pages/WithdrawalPage/WithdrawalPage";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Order />}></Route>
         <Route path="/cozinha" element={<Kitchen />}></Route>
+        <Route path="/retirada" element={<Withdrawal />}></Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/WithdrawalPage/WithdrawalPage.tsx
@@ -0,0 +1,6 @@
+
+export default function Withdrawal() {
+    return (
+        <h1>Retirada</h1>
+    )
+}
