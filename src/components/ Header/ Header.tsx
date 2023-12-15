e35250a611174e296d005a8acffbce9ea991fe0e
andressafirmino <andressafirmino78@gmail.com>
2023-12-15 20:54:43 -0300

    feat: link navigation enabled

--- src/components/ Header/ Header.tsx
+++ src/components/ Header/ Header.tsx
@@ -1,17 +1,18 @@
 import { HeaderContainer, HeaderLinks, HeaderLogo } from "./style";
 import Logo from '../../assets/Logo.png';
+import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
         <HeaderContainer>
             <HeaderLogo >
                 <img src={Logo} />
-                <p>fastfood</p>
+                <Link to="/"><p>fastfood</p></Link>
             </HeaderLogo>
             <HeaderLinks>
-                <div><p>Pedidos</p></div>
-                <div><p>Cozinha</p></div>
-                <div><p>Retirada</p></div>
+                <div><Link to="/"><p>Pedidos</p></Link></div>
+                <div><Link to="/cozinha"><p>Cozinha</p></Link></div>
+                <div><Link to="/retirada"><p>Retirada</p></Link></div>
             </HeaderLinks>
         </HeaderContainer>
     )
--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -4,6 +4,7 @@ const GlobalStyle = createGlobalStyle`
     * {
         box-sizing: border-box;
         font-family: 'Lexend Deca', sans-serif;
+        text-decoration: none;
     }
 `
 
