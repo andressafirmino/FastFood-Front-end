f43ef7074c0f68573f110d3c1c44f0a1f7f7bc00
andressafirmino <andressafirmino78@gmail.com>
2023-12-16 23:51:41 -0300

    feat: getting all categories, products and side dishes

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,17 +1,24 @@
 import { ProductContainer } from "./style";
 
-export default function Product({ props }: any) {
+interface ProductProps {
+    name: string;
+    price: number;
+    description: string;
+    image: string;
+}
 
-    const pricePtBr = (props.price / 100).toLocaleString("pt-BR", {
+export default function Product({ name, price, description, image }: ProductProps) {
+    
+    const pricePtBr = (price / 100).toLocaleString("pt-BR", {
         style: "currency",
         currency: "BRL",
     });
     return (
         <ProductContainer>
-            <div className="img"><img src={props.image} /></div>
+            <div className="img"><img src={image} /></div>
             <div className="information">
-                <p className="name">{props.name}</p>
-                <p className="description">{props.description}</p>
+                <p className="name">{name}</p>
+                <p className="description">{description}</p>
                 <p className="price">{pricePtBr}</p>
             </div>
         </ProductContainer>
--- src/pages/OrderPage/OrderPage.tsx
+++ src/pages/OrderPage/OrderPage.tsx
@@ -1,56 +1,16 @@
+/// <reference types="node" />
+import { useEffect } from "react";
 import Category from "../../components/Category/Category";
 import FinalizeOrder from "../../components/FinalizeOrder/FinalizeOrder";
 import Product from "../../components/Product/Product";
 import Search from "../../components/Search/Search";
 import { CategoriesContainer, OrderContainer, ProductsContainer, Subtitle, Title } from "./style";
+import axios from "axios";
 
 export default function Order() {
 
     const products = [
-        {
-            name: 'X-burguer',
-            price: 3400,
-            description: 'Hambúguer, pão, cebola, queijo, alface e tomate',
-            image: 'https://static.vecteezy.com/system/resources/previews/022/598/800/non_2x/tasty-beef-burger-png.png',
-            category: 'SNACK',
-        },
-        {
-            name: 'Coca-cola',
-            price: 400,
-            description: '350 ml',
-            image:
-                'https://s3-sa-east-1.amazonaws.com/deliveryon-uploads/products/texaslanches/141_5870274644f2a.png',
-            category: 'DRINK',
-        },
-        {
-            name: 'Combo x-burguer',
-            price: 4400,
-            description: '1 x-buguer, 1 batata-frita pequena e um refrigerante de 350ml',
-            image: 'https://static.vecteezy.com/system/resources/previews/022/598/800/non_2x/tasty-beef-burger-png.png',
-            category: 'COMBO',
-        },
-        {
-            name: 'Pudim',
-            price: 1000,
-            description: 'Pudim de doce de leite',
-            image: 'https://static.vecteezy.com/system/resources/previews/021/216/948/original/condensed-milk-pudding-with-eggs-png.png',
-            category: 'DESSERT',
-        },
-        {
-            name: 'X-burguer',
-            price: 3400,
-            description: 'Hambúguer, pão, cebola, queijo, alface e tomate',
-            image: 'https://static.vecteezy.com/system/resources/previews/022/598/800/non_2x/tasty-beef-burger-png.png',
-            category: 'SNACK',
-        },
-        {
-            name: 'Coca-cola',
-            price: 400,
-            description: '350 ml',
-            image:
-                'https://w7.pngwing.com/pngs/922/962/png-transparent-coca-cola-fizzy-drinks-diet-coke-fanta-coca-cola-cola-cola-wars-aluminum-can.png',
-            category: 'DRINK',
-        },
+       
         {
             name: 'Combo x-burguer',
             price: 4400,
@@ -60,7 +20,12 @@ export default function Order() {
         },
 
     ]
-
+    useEffect(() => {        
+        const url = `${import.meta.env.VITE_API_URL}/`;
+        axios.get(url)
+            .then(response => console.log(response.data))
+            .catch(e => console.log(e.message))
+    }, []);
     return (
         <OrderContainer>
             <Search></Search>
@@ -75,9 +40,9 @@ export default function Order() {
 
             <Subtitle>Selecione um produto para adicionar ao seu pedido</Subtitle>
 
-            <ProductsContainer>
-                {products.map(props => <Product props={props} />)}
-            </ProductsContainer>
+            {<ProductsContainer>
+                {products.map((prod, i)=> <Product key={i} {...prod} />)}
+            </ProductsContainer>}
 
             <FinalizeOrder></FinalizeOrder>
         </OrderContainer>
