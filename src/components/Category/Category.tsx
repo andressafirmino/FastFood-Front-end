b03f05938a08e07dcc726d9ec00c5193a4f9c252
andressafirmino <andressafirmino78@gmail.com>
2023-12-17 02:23:24 -0300

    feat: implementation of the product filter by category

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -1,12 +1,16 @@
 import axios from "axios";
 import { CategoryType } from "../../protocols";
 import { CategoryContainer } from "./style";
+import { useContext } from "react";
+import { ProductContext } from "../../context/products";
 
-export default function Category({name, image, category}: CategoryType ) {
+export default function Category({ name, image, category }: CategoryType) {
+    const { setProducts } = useContext(ProductContext);
+    
     function getProductsByCategory(category: string) {
         const url = `${import.meta.env.VITE_API_URL}/produtos/${category}`
         axios.get(url)
-            .then(response => console.log(response.data))
+            .then(response => setProducts(response.data))
             .catch(e => console.log(e.message))
     }
     return (
